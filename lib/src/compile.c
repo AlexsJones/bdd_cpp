c37878656f2976909a13d66e5b609899cc5ca100
AlexsJones <alexsimonjones@gmail.com>
2013-11-15 18:42:48 +0000

    working on compilation

---
+++ lib/src/compile.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  compile.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/15/13 18:40:14
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "compile.h"
+
+#define COMPILER "gcc"
+#define DEBUG_FLAGS " -g -lm "
+
+int compile_test(char *fpath)
+{
+	//look step_definitions for pickle.cmp for .c implementation file path references
+	
+	//compile recursively with pickle.cmp
+	
+
+	return 0;
+}
---
+++ lib/src/compile.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  compile.h
+ *
+ *    Description:  compiles test files 
+ *
+ *        Version:  1.0
+ *        Created:  11/15/13 18:39:32
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  jonesax@hush.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef __COMPILE_H__
+#define __COMPILE_H__
+
+int compile_test(char *fpath);
+
+#endif
