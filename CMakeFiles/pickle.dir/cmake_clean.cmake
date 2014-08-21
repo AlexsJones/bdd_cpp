20f4c6cfda61d32563682ab299191fdfc09b861c
AlexsJones <jonesax@hush.com>
2014-08-21 07:41:43 +0100

    updated for new jnxlibc drop

---
+++ CMakeFiles/pickle.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pickle.dir/lib/src/compile.c.o"
+  "CMakeFiles/pickle.dir/lib/src/executor.c.o"
+  "CMakeFiles/pickle.dir/lib/src/filesys.c.o"
+  "CMakeFiles/pickle.dir/lib/src/main.c.o"
+  "CMakeFiles/pickle.dir/lib/src/parser.c.o"
+  "CMakeFiles/pickle.dir/lib/src/scribe.c.o"
+  "pickle.pdb"
+  "pickle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pickle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
